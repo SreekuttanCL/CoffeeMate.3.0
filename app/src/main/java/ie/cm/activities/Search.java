d4c9a501547079b8b4c74e43af6841896da8853f
sreekuttancl <sreekuttancl12@gmail.com>
2019-07-23 14:25:02 -0400

    in class assignment

---
+++ app/src/main/java/ie/cm/activities/Search.java
@@ -0,0 +1,26 @@
+package ie.cm.activities;
+
+import ie.cm.R;
+import ie.cm.fragments.CoffeeFragment;
+import ie.cm.fragments.SearchFragment;
+
+import android.os.Bundle;
+
+public class Search extends Base {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.search);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		//coffeeFragment = CoffeeFragment.newInstance(); //get a new Fragment instance
+		coffeeFragment = SearchFragment.newInstance();
+		getFragmentManager().beginTransaction().replace(R.id.fragment_layout, coffeeFragment) .commit();
+
+
+	}
+
+}
